27318786f077d491c9cbc0c9d99e5781d9a9ef83
lread <lee@dlread.com>
2024-09-07 09:46:52 -0400

    Add `clojure.tools.reader` vars
    For compatibility with new tests in
    https://github.com/clj-commons/rewrite-clj/pull/306, add
    `clojure.tools-reader`:
    - `read-string`
    - `*read-eval*`

--- src/sci_test/impl/extras.clj
+++ src/sci_test/impl/extras.clj
@@ -4,11 +4,17 @@
    [clojure.java.io :as io]
    [clojure.tools.reader :as tr]
    [clojure.tools.namespace.find :as ns-find]
-   [clojure.tools.reader.edn :as edn]))
+   [clojure.tools.reader.edn :as edn]
+   [sci.core :as sci]))
+
+(def reader-ns (sci/create-ns 'clojure.tools.reader nil))
+(def read-eval (sci/new-dynamic-var '*read-eval* true {:ns reader-ns}))
 
 (def namespaces
   {'clojure.java.io {'file io/file}
    'clojure.tools.namespace.find {'find-namespaces ns-find/find-namespaces
                                   'clj ns-find/clj}
-   'clojure.tools.reader {'*alias-map* tr/*alias-map*}
+   'clojure.tools.reader {'*alias-map* tr/*alias-map*
+                          '*read-eval* read-eval
+                          'read-string tr/read-string}
    'clojure.tools.reader.edn {'read-string edn/read-string}})
